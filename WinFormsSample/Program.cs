9bdf6e3e90ff4efd2bfaab67a6eb2930479b79fa
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-27 17:50:35 +0300

    Update Program.cs use bootrapper

--- WinFormsSample/Program.cs
+++ WinFormsSample/Program.cs
@@ -17,7 +17,9 @@ namespace WinFormsSample
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
         }
     }
 }
