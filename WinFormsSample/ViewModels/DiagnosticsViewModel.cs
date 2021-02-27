def04d6396f5983d082c9a1b55526e12e863c29e
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-27 17:51:39 +0300

    Add ViewModels of pages

---
+++ WinFormsSample/ViewModels/DiagnosticsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsSample.ViewModels
+{
+    public class DiagnosticsViewModel : ReactiveObject, IRoutableViewModel
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => this.RaiseAndSetIfChanged(ref _title, value);
+        }
+
+        public string UrlPathSegment { get; protected set; }
+        public IScreen HostScreen { get; protected set; }
+
+        public DiagnosticsViewModel()
+        {
+            Title = "Diagnostics";
+        }
+    }
+}
